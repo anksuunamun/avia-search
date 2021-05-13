779f4858073823d424dec6e473bf94d6bbf60c08
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-13 11:31:20 +0300

    feat: add function for getting flights data

---
+++ src/DAL/flights.ts
@@ -0,0 +1,30 @@
+import {v1} from 'uuid';
+// @ts-ignore
+import data from './flights.json';
+
+
+export const getFlights = () => {
+    // @ts-ignore
+    return data.result.flights.map((flight: any) => {
+        const routes = flight.flight.legs.map((leg: any) => {
+            const segment = leg.segments[0];
+            return {
+                airline: segment.airline,
+                departureCity: segment.departureCity,
+                departureAirport: segment.departureAirport,
+                departureDate: segment.departureDate,
+                arrivalCity: segment.arrivalCity,
+                arrivalAirport: segment.arrivalAirport,
+                arrivalDate: segment.arrivalDate,
+                stops: segment.stops,
+                key: v1()
+            }
+        })
+        return {
+            price: flight.flight.price.total.amount,
+            flightCaption: flight.flight.carrier.caption,
+            routes: routes,
+            key: v1()
+        }
+    });
+}
