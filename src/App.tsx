8f1de9b25017e64c4952b3b054eb77cac0c393a3
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-13 11:34:43 +0300

    feat: add setting data in local state and main app components

--- src/App.tsx
+++ src/App.tsx
@@ -24,24 +24,30 @@ export type FlightsType = {
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [flights, setFlights] = useState<Array<FlightsType>>()
+
+    useEffect(() => {
+        const flights = getFlights()
+        setFlights(flights)
+    }, [])
+
+    const flightsItems = flights?.map(item => <Flight {...item} />)
+
+    if (!flights) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div className="App">
+            <div>
+                <Filters/>
+            </div>
+            <div>
+                {flightsItems}
+            </div>
+        </div>
+    );
 }
 
 export default App;
