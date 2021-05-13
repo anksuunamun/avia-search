652089d4dc8ec0a703d11672b1bec7e65f41eef3
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-13 15:33:15 +0300

    feat: implement function for calculating flight duration

--- src/utils/helper-functions.ts
+++ src/utils/helper-functions.ts
@@ -4,3 +4,4 @@ export const addZero = (time: number) => {
     return time < 10 ? `0${time}` : time;
 }
 
+export const getFlightDuration = (flight: FlightsType) => flight.routes.reduce((acc, current) => acc + current.duration, 0)
