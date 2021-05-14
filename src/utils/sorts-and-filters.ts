952cc55f7364c484f6487467c355c3270ae886eb
anksuunamun <shishkina_veronika_10@mail.ru>
2021-05-14 15:34:05 +0300

    feat: implement function to filter by airline, stops and price range simultaneously

--- src/utils/sorts-and-filters.ts
+++ src/utils/sorts-and-filters.ts
@@ -12,17 +12,47 @@ export const sortByTravelTime = (flights: Array<FlightsType>) => {
     return [...flights].sort((flight1, flight2) => getFlightDuration(flight1) - getFlightDuration(flight2))
 }
 
+
 export const filterByStopsCount = (flights: Array<FlightsType>, stopsCount: number) => {
     return [...flights].filter(flight => {
         let stops = flight.routes.reduce((acc, current) => Math.max(acc, current.stops), 0)
-        return stops === stopsCount;
+        return stops <= stopsCount;
     })
 }
 
-export const filterByMinMaxPrice = (flights: Array<FlightsType>, min: number, max: number) => {
-    let result = flights.filter(flight => (+flight.price >= min && +flight.price <= max))
+export const filterByCompanyName = (flights: Array<FlightsType>, companies: string[]) => {
+    return companies.length > 0 ? flights.filter(flight => companies.some(company => company === flight.flightCaption)) : flights;
 }
 
-export const filterByCompanyName = (flights: Array<FlightsType>, companies: string[]) => {
-    return flights.filter(flight => companies.some(company => company === flight.flightCaption))
+export const getFilteredFlights = (flights: Array<FlightsType>, stopsCount: number[], min: number, max: number, companies: string[]) => {
+    return filterByCompanyName(flights.filter(flight => {
+        let stops = flight.routes.reduce((acc, current) => Math.max(acc, current.stops), 0);
+        let rangeFilterResult = (+flight.price >= min && +flight.price <= max);
+        return stops <= Math.max(stopsCount[0], stopsCount[1]) && rangeFilterResult
+    }), companies)
 }
+
+// let flights = [{flightCaption: "LOT Polish Airlines",
+//     key: "ea4bf134-b49e-11eb-8a51-0906203a6a54",
+//     price: "21049",
+//     routes: [{
+//         airline: {uid: 'LO', caption: 'LOT Polish Airlines', airlineCode: 'LO'},
+//         arrivalAirport: {uid: 'WAW', caption: 'ВАРШАВА (ФРЕДЕРИК ШОПЕН)'},
+//         arrivalCity: {uid: 'WAW', caption: 'ВАРШАВА (ФРЕДЕРИК ШОПЕН)'},
+//         arrivalDate: '2020-08-18T21:55:00',
+//         departureAirport: {uid: 'SVO', caption: 'ШЕРЕМЕТЬЕВО'},
+//         departureCity: {uid: 'MOW', caption: 'Москва'},
+//         departureDate: '2020-08-18T20:40:00',
+//         duration: 885,
+//         key: 'ea4bf132-b49e-11eb-8a51-0906203a6a54',
+//         stops: 0
+//     } ]}]
+
+// const getFilteredFlights = (flights, stopsCount, min, max, companies) => {
+//     return flights
+//         .filter(flight => {
+//             let stops = flight.routes.reduce((acc, current) => Math.max(acc, current.stops), 0);
+//             let rangeFilterResult = (+flight.price >= min && +flight.price <= max);
+//             return stops <= stopsCount && rangeFilterResult
+//         })
+// }
